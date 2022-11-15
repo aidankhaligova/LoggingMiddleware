e9babd506b70dd43dda8e4dc3ec0107ba3c2f5d9
aidankhaligova <aydan.abdulkhaliqova@gts.az>
2022-11-15 14:47:40 +0400

    Add project files.

---
+++ TestLogging/LoggingMiddleware/RequestResponseLoggingMiddlewareExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace TestLogging.LoggingMiddleware
+{
+    public static class RequestResponseLoggingMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseRequestResponseLogging(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<RequestResponseLoggingMiddleware>();
+        }
+    }
+}
